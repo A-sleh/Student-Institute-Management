dbdcd479b3f50e5564ad30749c329dc07c7380d9
RabooV01 <rabii961ra@gmail.com>
2024-08-20 14:21:39 +0300

    One to one mapping For StudentModel

--- Back-End/MiniAPI/DataAcess/Data/StudentData.cs
+++ Back-End/MiniAPI/DataAcess/Data/StudentData.cs
@@ -16,11 +16,30 @@ public class StudentData : IStudentData
     {
         this._db = db;
     }
-    public Task<IEnumerable<StudentModel>> GetStudents() =>
-         _db.LoadData<StudentModel, dynamic>("dbo.StudentGetAll", new { });
+    public Task<IEnumerable<StudentModel>> GetStudents()
+    {
+        var res = _db.LoadData<StudentModel, dynamic, ClassModel>(
+            "dbo.StudentGetAll",
+            parameters: new { },
+            (StudentModel Student, ClassModel Class) =>
+            {
+                Student.Class = Class;
+                return Student;
+            },
+            splitOn: "ClassId"
+            );
+        return res;
+    }
+
     public async Task<StudentModel?> GetStudentByID(int id)
     {
-        var res = await _db.LoadData<StudentModel, dynamic>("dbo.StudentGet", new { Id = id });
+        var res = await _db.LoadData<StudentModel, dynamic, ClassModel>("dbo.StudentGet",
+            parameters: new { Id = id },
+            (StudentModel Student, ClassModel Class) => {
+            Student.Class = Class;
+            return Student;
+        },
+            splitOn: "ClassId");
         return res.FirstOrDefault();
     }
     public Task InsertStudent(StudentModel student) =>
@@ -32,7 +51,7 @@ public class StudentData : IStudentData
             student.FatherName,
             student.Birthdate,
             student.Phone,
-            student.ClassId,
+            ClassId = student.Class?.ClassId,
             student.MissedDays,
             student.BillRequired
         });
@@ -45,7 +64,7 @@ public class StudentData : IStudentData
             student.FatherName,
             student.Birthdate,
             student.Phone,
-            student.ClassId,
+            ClassId = student.Class?.ClassId,
             student.MissedDays,
             student.BillRequired
         });
--- Back-End/MiniAPI/UserDB/StoredProcedures/StudentGet.sql
+++ Back-End/MiniAPI/UserDB/StoredProcedures/StudentGet.sql
@@ -1,7 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[StudentGet]
-	@Id int
+@Id int
 AS
-BEGIN
-	SELECT * FROM student
-	WHERE id = @Id
-END
+begin
+	SELECT s.id, name, lastName, fatherName, birthdate, phone, missedDays, billRequired, s.classId as ClassId, c.title, c.capacity, c.gender, c.grade
+	FROM student s left outer join class c on s.classId = c.id
+	WHERE s.id = @Id;
+end
