fc32b0ee6523e38402fe70c6b1cc44e06e05d01d
RabooV01 <rabii961ra@gmail.com>
2024-10-24 16:12:56 +0300

    added new TeacherSubject Endpoint to get all teachers subjects filterable by grade

--- Back-End/MiniAPI/DataAcess/Models/TeacherSubjectModel.cs
+++ Back-End/MiniAPI/DataAcess/Models/TeacherSubjectModel.cs
@@ -9,7 +9,7 @@ namespace DataAcess.Models
     public class TeacherSubjectModel
     {
         public int TeacherSubjectId {  get; set; }
-        public int TeacherId {  get; set; }
+        public TeacherModel? Teacher {  get; set; }
         public SubjectModel? Subject {  get; set; }
         public int Salary {  get; set; }
         public List<ClassModel?> Classes { get; set; } = [];
---
+++ Back-End/MiniAPI/UserDB/StoredProcedures/TeacherSubjectGetAll.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[TeacherSubjectGetAll]
+	@grade varchar(15) null
+AS
+	SELECT 
+	ts.Id as TeacherSubjectId, ts.Salary,
+	t.Id as TeacherId, t.Name, t.LastName, t.Phone,
+	s.Id as SubjectId, s.Subject, s.Grade, s.MaximumMark
+	FROM TeacherSubject ts
+	JOIN Teacher t ON ts.TeacherId = t.Id
+	JOIN Subject s ON ts.SubjectId = s.Id
+	WHERE s.Grade = @grade OR @grade IS NULL
+RETURN 0
