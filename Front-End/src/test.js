2135404bf27b5f37fc812b86c04db94d08b26f4e
A-sleh <businessabdo9@gmail.com>
2024-10-31 18:40:55 +0400

    some change

---
+++ Front-End/src/test.js
@@ -0,0 +1,189 @@
+const data = {
+    "quizAverage": null,
+    "examAverage": 93,
+    "pureMark": 2254,
+    "student": {
+      "studentId": 1,
+      "name": "Ramez",
+      "lastName": "A.",
+      "fatherName": "Mohamad",
+      "birthdate": null,
+      "phone": null,
+      "missedDays": 0,
+      "billRequired": 0,
+      "class": null,
+      "testMark": [
+        {
+          "testMarkId": 1,
+          "student": null,
+          "test": {
+            "testId": 1,
+            "report": null,
+            "subject": {
+              "subjectId": 1,
+              "subject": "Math",
+              "grade": "bachelor",
+              "maximumMark": 600
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/15/2024 00:00:00",
+            "correctionDate": "08/20/2024 00:00:00"
+          },
+          "mark": 600
+        },
+        {
+          "testMarkId": 2,
+          "student": null,
+          "test": {
+            "testId": 2,
+            "report": null,
+            "subject": {
+              "subjectId": 2,
+              "subject": "Arabic",
+              "grade": "bachelor",
+              "maximumMark": 400
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/17/2024 00:00:00",
+            "correctionDate": "08/22/2024 00:00:00"
+          },
+          "mark": 400
+        },
+        {
+          "testMarkId": 3,
+          "student": null,
+          "test": {
+            "testId": 3,
+            "report": null,
+            "subject": {
+              "subjectId": 3,
+              "subject": "Science",
+              "grade": "bachelor",
+              "maximumMark": 300
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/18/2024 00:00:00",
+            "correctionDate": "08/23/2024 00:00:00"
+          },
+          "mark": 280
+        },
+        {
+          "testMarkId": 4,
+          "student": null,
+          "test": {
+            "testId": 4,
+            "report": null,
+            "subject": {
+              "subjectId": 4,
+              "subject": "Physics",
+              "grade": "bachelor",
+              "maximumMark": 400
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/19/2024 00:00:00",
+            "correctionDate": "08/25/2024 00:00:00"
+          },
+          "mark": 360
+        },
+        {
+          "testMarkId": 5,
+          "student": null,
+          "test": {
+            "testId": 5,
+            "report": null,
+            "subject": {
+              "subjectId": 5,
+              "subject": "English",
+              "grade": "bachelor",
+              "maximumMark": 300
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/20/2024 00:00:00",
+            "correctionDate": "08/25/2024 00:00:00"
+          },
+          "mark": 280
+        },
+        {
+          "testMarkId": 6,
+          "student": null,
+          "test": {
+            "testId": 6,
+            "report": null,
+            "subject": {
+              "subjectId": 6,
+              "subject": "France",
+              "grade": "bachelor",
+              "maximumMark": 300
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/22/2024 00:00:00",
+            "correctionDate": "08/26/2024 00:00:00"
+          },
+          "mark": 288
+        },
+        {
+          "testMarkId": 7,
+          "student": null,
+          "test": {
+            "testId": 7,
+            "report": null,
+            "subject": {
+              "subjectId": 7,
+              "subject": "Chimestry",
+              "grade": "bachelor",
+              "maximumMark": 200
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/24/2024 00:00:00",
+            "correctionDate": "08/28/2024 00:00:00"
+          },
+          "mark": 150
+        },
+        {
+          "testMarkId": 8,
+          "student": null,
+          "test": {
+            "testId": 8,
+            "report": null,
+            "subject": {
+              "subjectId": 8,
+              "subject": "Religion",
+              "grade": "bachelor",
+              "maximumMark": 200
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/27/2024 00:00:00",
+            "correctionDate": "08/30/2024 00:00:00"
+          },
+          "mark": 190
+        },
+        {
+          "testMarkId": 9,
+          "student": null,
+          "test": {
+            "testId": 9,
+            "report": null,
+            "subject": {
+              "subjectId": 9,
+              "subject": "History",
+              "grade": "bachelor",
+              "maximumMark": 200
+            },
+            "testType": "exam",
+            "title": null,
+            "date": "08/28/2024 00:00:00",
+            "correctionDate": "08/31/2024 00:00:00"
+          },
+          "mark": 176
+        }
+      ]
+    }
+  }
