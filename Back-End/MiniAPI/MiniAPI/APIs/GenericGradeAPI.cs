6a6c34f1397c086a34b175e382f0eb981654a4e5
RabooV01 <rabii961ra@gmail.com>
2024-11-20 13:28:25 +0200

    merge2

---
+++ Back-End/MiniAPI/MiniAPI/APIs/GenericGradeAPI.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Runtime.CompilerServices;
+
+namespace MiniAPI.APIs
+{
+    public static class GenericGradeAPI
+    {
+        public static void ConfigureGradeAPI(this WebApplication app)
+        {
+
+        }
+
+        private static async Task<IResult> GetGrades()
+        {
+            return  Results.Ok("s");
+        }
+    }
+}
