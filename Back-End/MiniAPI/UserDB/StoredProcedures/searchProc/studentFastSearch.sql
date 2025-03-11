814369e8a7f5ec2ca8848ae330329b4fabf7d3a8
RabooV01 <rabii961ra@gmail.com>
2025-03-11 17:40:53 +0200

    feat/init sql student search

---
+++ Back-End/MiniAPI/UserDB/StoredProcedures/searchProc/studentFastSearch.sql
@@ -0,0 +1,21 @@
+﻿-- search students by name, lastName and class Title
+
+CREATE PROCEDURE [dbo].[studentFastSearch]
+	@content NVARCHAR(512) = '',
+	@classId int = null,
+	@PageSize int = 5,
+	@Page INT = 1
+AS
+	WITH studentAbsences AS ((SELECT studentId, COUNT(*) as MissedDays FROM absence GROUP BY studentId))
+	SELECT s.id as StudentId, name, lastName, fatherName, birthdate, phone, billRequired, COALESCE(a.MissedDays, 0) as MissedDays,
+	c.id as ClassId, c.title, c.gradeId, g.grade, c.gender, c.capacity
+	FROM Student s left join Class c on s.classId = c.id 
+	LEFT JOIN Grade g ON c.gradeId = g.gradeId
+	LEFT JOIN studentAbsences a ON s.id = a.studentId
+	WHERE (@classId IS NULL OR s.classId = @classId)
+	AND ( name LIKE (CONCAT(N'%', @content, N'%'))
+	OR lastName LIKE (CONCAT(N'%', @content, N'%')))
+	ORDER BY name
+	OFFSET ((@Page-1)*@pageSize) ROWS
+	FETCH NEXT @PageSize ROWS ONLY
+RETURN 0
---
+++ Back-End/MiniAPI/UserDB/Types/StudentClassTableType.sql
@@ -0,0 +1,19 @@
+﻿-- Good for retrieving data from StudentGet/StudentGetAll Proc
+-- Gives students and related class
+CREATE TYPE [dbo].[StudentClassTableType] AS TABLE
+(
+	StudentId INT,
+	name NVARCHAR(128),
+	lastName NVARCHAR(128),
+	fatherName NVARCHAR(128),
+	birthDate DATE,
+	Phone VARCHAR(15),
+	BillRequired INT,
+	MissedDays INT,
+	classId INT,
+	title NVARCHAR(128),
+	gradeId INT,
+	grade NVARCHAR(128),
+	gender NVARCHAR(128),
+	capacity INT
+)
