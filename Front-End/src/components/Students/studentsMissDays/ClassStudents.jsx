23d378fddbee899eeaa81436261185d401f47a22
A-sleh <146457544+A-sleh@users.noreply.github.com>
2025-03-13 14:33:13 +0300

    per( student missdays page ) : resolve one error while the table are rendering ,and merge the lastest version of backEnd

--- Front-End/src/components/Students/studentsMissDays/ClassStudents.jsx
+++ Front-End/src/components/Students/studentsMissDays/ClassStudents.jsx
@@ -58,9 +58,10 @@ export default function ClassStudents() {
           english: 'Select'
         } ,
         id: "selection",
-        Cell: ({ row }) => (
-          <input type="checkbox" {...row.getToggleRowSelectedProps()} />
-        ),
+        Cell: ({ row }) => {
+          const {checked,onChange} = row.getToggleRowSelectedProps() 
+          return <input type="checkbox"  checked={checked} onChange={(e)=>onChange(e)} />
+        },
       },
 
     ],[])
