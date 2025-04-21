e10e8e4b4228b277a5a2d8d66425044ff81bb083
A-sleh <146457544+A-sleh@users.noreply.github.com>
2025-04-21 14:15:20 +0300

    per ( login section ) : un sotre admin data in localstorage

--- Front-End/src/Redux/Reducers/adminReducer.js
+++ Front-End/src/Redux/Reducers/adminReducer.js
@@ -2,18 +2,10 @@ import { ADMINLOGUNG, ADMINLOGUNGOUT} from "../actions/type";
 
 const adminReducer = ( state = {isAdmin : false , adminName: '' } , action ) => {
 
-    
-    const storedState = JSON.parse( decodeURIComponent(localStorage.getItem('admin')))
-    if(storedState != undefined ) {
-        state = storedState
-    }
-
     switch(action.type) {
         case ADMINLOGUNG :
-            localStorage.setItem('admin',encodeURIComponent(JSON.stringify({isAdmin: action.payload.isAdmin ,adminName : action.payload.adminName })))
             return {isAdmin: action.payload.isAdmin ,adminName : action.payload.adminName }
         case ADMINLOGUNGOUT :
-            localStorage.removeItem('admin')
             return {isAdmin: action.payload , adminName: ''}
         default : 
             return state ;
