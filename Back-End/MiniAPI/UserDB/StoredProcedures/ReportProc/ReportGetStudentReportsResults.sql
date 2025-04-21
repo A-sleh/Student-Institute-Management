c119345ed0a8372e954d2845d448a229d5e6a455
RabooV01 <rabii961ra@gmail.com>
2025-04-21 18:43:38 +0300

    refactor(sql): fixed duplicated report when quiz on it

--- Back-End/MiniAPI/UserDB/StoredProcedures/ReportProc/ReportGetStudentReportsResults.sql
+++ Back-End/MiniAPI/UserDB/StoredProcedures/ReportProc/ReportGetStudentReportsResults.sql
@@ -6,6 +6,6 @@ AS
 	SUM(rm.totalMark) as totalMark,
 	LEFT(SUM(rm.mark)*100.0/SUM(rm.totalMark), 5) as markPercentage
 	FROM TestsMarksForEachReport rm
-	WHERE rm.StudentId = @studentId
+	WHERE rm.StudentId = @studentId AND TestType not like 'quiz'
 	GROUP BY rm.Id, rm.ReportTitle, rm.StartDate, rm.FinishDate, rm.TestType
 RETURN 0
